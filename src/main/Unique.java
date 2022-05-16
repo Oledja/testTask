a5cf3939a3e74a9aa7663d79b2d398cfd6294d77
oleg-prylipko <oledja1987@gmail.com>
2022-05-16 22:24:13 +0300

    change

--- src/main/Unique.java
+++ src/main/Unique.java
@@ -9,23 +9,23 @@ public class Unique {
 
     public String[] findAllCombinationWithDots(String str) {
         checkArguments(str);
-        List<String> uniqueCombinations = findUniqueCombinations(str, DELIMITER);
+        List<String> uniqueCombinations = findUniqueCombinations(str);
         uniqueCombinations.add(str);
         return uniqueCombinations.toArray(new String[uniqueCombinations.size()]);
     }
 
-    private List<String> findUniqueCombinations(String str, String delimiter) {
+    private List<String> findUniqueCombinations(String str) {
         List<String> uniqStrings = new ArrayList<>();
         int index = 1;
 
-        if (str.contains(delimiter)){
-            index = str.lastIndexOf(delimiter)  + 2;
+        if (str.contains(DELIMITER)){
+            index = str.lastIndexOf(DELIMITER)  + 2;
         }
 
         for (int i = index; i <= str.length() - 1; i++) {
-            StringBuilder unique = new StringBuilder(str).insert(i, ".");
+            StringBuilder unique = new StringBuilder(str).insert(i, DELIMITER);
             uniqStrings.add(unique.toString());
-            uniqStrings.addAll(findUniqueCombinations(unique.toString(), delimiter));
+            uniqStrings.addAll(findUniqueCombinations(unique.toString()));
         }
         return uniqStrings;
     }
