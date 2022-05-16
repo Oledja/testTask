6d82623567ce580d82d0ea310e990341c2d33ae8
oleg-prylipko <oledja1987@gmail.com>
2022-05-16 17:28:38 +0300

    change

--- src/main/Unique.java
+++ src/main/Unique.java
@@ -5,41 +5,41 @@ import java.util.List;
 
 public class Unique {
 
-    public String[] findAllCombinationWithDots(String string) {
-        checkArguments(string);
-        List<String> strings = findUniqueCombinations(string);
-        strings.add(string);
+    public String[] findAllCombinationWithDots(String str) {
+        checkArguments(str);
+        List<String> strings = findUniqueCombinations(str);
+        strings.add(str);
         return convertListToStringArray(strings);
     }
 
     private List<String> findUniqueCombinations(String str) {
         List<String> uniqStrings = new ArrayList<>();
-        int arg = 1;
+        int index = 1;
 
         if (str.contains(".")){
-            arg = str.lastIndexOf(".")  + 2;
+            index = str.lastIndexOf(".")  + 2;
         }
 
-        for (int i = arg; i <= str.length() - 1; i++) {
-            StringBuilder newWord = new StringBuilder(str).insert(i, ".");
-            uniqStrings.add(newWord.toString());
-            uniqStrings.addAll(findUniqueCombinations(newWord.toString()));
+        for (int i = index; i <= str.length() - 1; i++) {
+            StringBuilder unique = new StringBuilder(str).insert(i, ".");
+            uniqStrings.add(unique.toString());
+            uniqStrings.addAll(findUniqueCombinations(unique.toString()));
         }
         return uniqStrings;
     }
 
-    private String[] convertListToStringArray(List<String> list) {
-        String[] strings = new String[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            strings[i] = list.get(i);
+    private String[] convertListToStringArray(List<String> strings) {
+        String[] result = new String[strings.size()];
+        for (int i = 0; i < strings.size(); i++) {
+            result[i] = strings.get(i);
             }
-        return strings;
+        return result;
     }
 
-    private void checkArguments(String arg) {
-        if (!arg.matches("[a-zA-Z]+")) {
+    private void checkArguments(String str) {
+        if (!str.matches("[a-zA-Z]+")) {
             throw new IllegalArgumentException(String.format(
-                    "Argument can only contain letters, arg = '%s'", arg));
+                    "Argument can only contain letters, arg = '%s'", str));
         }
     }
 
