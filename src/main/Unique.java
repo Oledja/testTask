cf2073a8cf8c085fe9e2cfd65c2d28e2d9f3fb9c
oleg-prylipko <oledja1987@gmail.com>
2022-05-16 18:02:40 +0300

    change

--- src/main/Unique.java
+++ src/main/Unique.java
@@ -5,35 +5,29 @@ import java.util.List;
 
 public class Unique {
 
+    private static final String DELIMITER = ".";
+
     public String[] findAllCombinationWithDots(String str) {
         checkArguments(str);
-        List<String> strings = findUniqueCombinations(str);
-        strings.add(str);
-        return convertListToStringArray(strings);
+        List<String> uniqueCombinations = findUniqueCombinations(str, DELIMITER);
+        uniqueCombinations.add(str);
+        return uniqueCombinations.toArray(new String[uniqueCombinations.size()]);
     }
 
-    private List<String> findUniqueCombinations(String str) {
+    private List<String> findUniqueCombinations(String str, String delimiter) {
         List<String> uniqStrings = new ArrayList<>();
         int index = 1;
 
-        if (str.contains(".")){
-            index = str.lastIndexOf(".")  + 2;
+        if (str.contains(delimiter)){
+            index = str.lastIndexOf(delimiter)  + 2;
         }
 
         for (int i = index; i <= str.length() - 1; i++) {
             StringBuilder unique = new StringBuilder(str).insert(i, ".");
             uniqStrings.add(unique.toString());
-            uniqStrings.addAll(findUniqueCombinations(unique.toString()));
+            uniqStrings.addAll(findUniqueCombinations(unique.toString(), delimiter));
         }
         return uniqStrings;
-    }
-
-    private String[] convertListToStringArray(List<String> strings) {
-        String[] result = new String[strings.size()];
-        for (int i = 0; i < strings.size(); i++) {
-            result[i] = strings.get(i);
-            }
-        return result;
     }
 
     private void checkArguments(String str) {
