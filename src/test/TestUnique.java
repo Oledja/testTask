455e4829029c68efb89fce5dbeb166ff025ec14d
oleg-prylipko <oledja1987@gmail.com>
2022-05-19 14:26:58 +0300

    change

--- src/test/TestUnique.java
+++ src/test/TestUnique.java
@@ -3,16 +3,32 @@ package test;
 import main.Unique;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class TestUnique {
     public static void main(String[] args) {
         Unique unique = new Unique();
-        String testStr = "abcdefr";
+        String testStr = "abcdefrgdh";
 
-        String[] result = unique.findAllCombinationWithDots(testStr);
-        Set<String> setStr = new HashSet<>(Arrays.asList(result));
+        String[] arrStr = unique.findAllCombinationWithDots(testStr);
+        Set<String> setStr = new HashSet<>(Arrays.asList(arrStr));
+        int allUniqResultsForLength = getCountUniqueResults(testStr);
 
-        System.out.println(result.length == setStr.size());
+        boolean result = Objects.equals(setStr.size(), allUniqResultsForLength);
+        System.out.println(result);
+    }
+
+    private static int getCountUniqueResults(String str) {
+        int strLength = str.length();
+        if (strLength == 1) {
+            return 1;
+        }
+        int result = 2;
+        for (int i = 0; i < strLength - 2; i++) {
+            result = result * 2;
+        }
+        return result;
     }
 }
+//
